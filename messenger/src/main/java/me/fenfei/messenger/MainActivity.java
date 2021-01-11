770badc7e3921fe7af89b994510412fc9e6fc4f9
周玉岩 <1774329934@qq.com>
2021-01-11 11:50:03 +0800

    新模型开辟

---
+++ messenger/src/main/java/me/fenfei/messenger/MainActivity.java
@@ -0,0 +1,18 @@
+package me.fenfei.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * https://www.yuque.com/androidzhixin/androidzhixin/8b5835119d2bde1770d2a3bb7d33bcd9
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
